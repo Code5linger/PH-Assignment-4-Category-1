988a6a84df9ad44572f58e86b42f1d3b8da6abb4
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-02-17 21:22:18 +0600

    Added HTML to Check

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function cashOut(money) {
   if (typeof money !== 'number' || money < 0) {
+    // Insted of creating another if block I used Ternary Operator "||, &&"
     return 'Invalid';
   }
   return (money * 1.75) / 100;
