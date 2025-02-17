a9f1d23db0879e365271fadd38bc73989c587998
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-02-17 21:30:03 +0600

    // Problem-02 : Detect email  valid or not

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Problem-01 : Calculate the cash-out Charge
 function cashOut(money) {
   if (typeof money !== 'number' || money < 0) {
     // Insted of creating another if block I used Ternary Operator "||, &&"
@@ -5,3 +6,32 @@ function cashOut(money) {
   }
   return (money * 1.75) / 100;
 }
+
+// Problem-02 : Detect email  valid or not
+
+function validEmail(email) {
+  if (typeof email !== 'string') {
+    return 'Invalid';
+  }
+
+  const firstChar = email.charAt(0);
+  if ('.-_+@'.includes(firstChar)) {
+    return false;
+  }
+
+  if (!email.includes('@')) {
+    return false;
+  }
+
+  if (email.includes(' ')) {
+    return false;
+  }
+
+  if (!email.endsWith('.com')) {
+    return false;
+  }
+
+  return true;
+}
+
+// Problem-03 : Who will Win  ( 🥭Mango /🍌 Banana )
